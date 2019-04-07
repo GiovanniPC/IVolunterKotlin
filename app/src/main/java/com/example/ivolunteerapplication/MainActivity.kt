746f01e9dc453045b8673cafe79ac3ba64ca5ba1
Guilherme Dias <guilhermegdf2@hotmail.com>
2019-04-07 14:38:18 -0300

    first commit

---
+++ app/src/main/java/com/example/ivolunteerapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.ivolunteerapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botaoLogin = findViewById<Button>(R.id.button)
+        botaoLogin.setOnClickListener {onClickLogin() }
+
+    }
+
+    fun onClickLogin(){
+        val campoUsuario = findViewById<EditText>(R.id.editText)
+        val campoSenha = findViewById<EditText>(R.id.editText2)
+        val valorUsuario = campoUsuario.text.toString()
+        val valorSenha = campoSenha.text.toString()
+        val username: String = "aluno"
+        val password: String = "impacta"
+
+        if (valorSenha == password  && valorUsuario == username){
+            val intent = Intent(this, TelaInicialActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+        Toast.makeText(this, "Nome de usuario ou senha inválidos", Toast.LENGTH_LONG).show()
+    }
+    }
+}
---
+++ app/src/main/java/com/example/ivolunteerapplication/TelaInicialActivity.kt
@@ -0,0 +1,39 @@
+package com.example.ivolunteerapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class TelaInicialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        if(id == R.id.action_buscar){
+            Toast.makeText(this, "Clicou Buscar", Toast.LENGTH_LONG).show()
+        }
+        if(id == R.id.action_atualizar){
+            Toast.makeText(this, "Clicou Atualizar", Toast.LENGTH_LONG).show()
+        }
+        if(id == R.id.action_logout){
+
+            Toast.makeText(this, "Clicou Sair", Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
